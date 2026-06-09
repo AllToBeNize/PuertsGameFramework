3ffe814fb0f6fbbb41ee2e945ea2ef33fa2651f9
AllToBeNize <2184196946@qq.com>
2026-06-10 01:35:03 +0800

    新增setMap工具

---
+++ TypeScript/core/Utility.ts
@@ -0,0 +1,8 @@
+export function setMap<K, V>(map: Map<K, Array<V>>, key: K, value: V) {
+    let arr = map.get(key);
+    if (arr) {
+        arr.push(value);
+    } else {
+        map.set(key, [value]);
+    }
+}
