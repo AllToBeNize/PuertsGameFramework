91465918dba15f1b93b51ebe6167ced811249a24
AllToBeNize <2184196946@qq.com>
2026-06-18 01:06:16 +0800

    CreateBehaviours改名CreateBehaviour

--- PuertsGameFramework/TypeScript/core/component/CreateBehaviourComponent.ts
+++ PuertsGameFramework/TypeScript/core/component/CreateBehaviourComponent.ts
@@ -4,7 +4,7 @@ import { Behaviour } from "../Behaviour"
 import { BehaviourManager } from "../BehaviourManager"
 
 @bind(UE.PuertsGameFramework.Component.CreateBehaviourComponent.CreateBehaviourComponent_C)
-export class CreateBehavioursComponent extends Behaviour<UE.PuertsGameFramework.Component.CreateBehaviourComponent.CreateBehaviourComponent_C> {
+export class CreateBehaviourComponent extends Behaviour<UE.PuertsGameFramework.Component.CreateBehaviourComponent.CreateBehaviourComponent_C> {
     protected onBind(): void {
         for (let i = 0; i < this.u_object.Behaviours.Num(); i++) {
             const behaviourName = this.u_object.Behaviours.Get(i)
--- PuertsGameFramework/TypeScript/core/component/index.ts
+++ PuertsGameFramework/TypeScript/core/component/index.ts
@@ -1,1 +1,1 @@
-import "./CreateBehavioursComponent"
+import "./CreateBehaviourComponent"
